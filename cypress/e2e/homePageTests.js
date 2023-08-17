0f647c585447f03ef7588c74bb399306596369e3
Czajkowski <wiktor.czajkowski@capgemini.com>
2023-08-17 12:43:52 +0200

    merged shopping_tests to main and create 2 more test files for homePage and Product tests

---
+++ cypress/e2e/homePageTests.js
@@ -0,0 +1,15 @@
+/// <reference types="Cypress" />
+
+describe('Home Page basic Testing', () => {
+  it("check text on home page", () => {});
+
+  it("check about us footer", () => {});
+
+  it("check contac us footer", () => {});
+
+  it("check testimonials footer", () => {});
+
+  it("sign up for newsletter group", () => {});
+
+  it("check brand scrolling list", () => {});
+});
---
+++ cypress/e2e/productTests.js
@@ -0,0 +1,39 @@
+/// <reference types="Cypress" />
+
+describe('Testing products functionality', () => {
+  it("change quantity and add item to cart", () => {});
+
+  it("add item to wishlist", () => {});
+
+  it("remove item from wishlist", () => {});
+
+  it("print the item details", () => {});
+
+  it("check item details", () => {});
+
+  it("check item reviews", () => {});
+
+  it("check related products", () => {});
+
+  it("change currency and check prices of products", () => {});
+
+  it("check special offer - verify all products", () => {});
+
+  it("find the product from search field", () => {});
+
+  it("find all products related to the brand - click Gucci logo in Brand list on home page", () => {});
+
+  it("check featured products", () => {});
+
+  it("check latest products", () => {});
+
+  it("check bestsellers products", () => {});
+
+  it("check special products on home page", () => {});
+
+  it("change sort by - highest rating", () => {});
+
+  it("search the item - find no items matching to search criteria", () => {});
+
+  it("check the order details by finding by id and email", () => {});
+});
